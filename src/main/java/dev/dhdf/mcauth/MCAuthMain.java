d54ba259b5660aef1e382491d90c7b9ab23c6383
Dylan Hackworth <dhpf@pm.me>
2020-08-09 15:05:42 -0500

    Removed new thread

--- src/main/java/dev/dhdf/mcauth/MCAuthMain.java
+++ src/main/java/dev/dhdf/mcauth/MCAuthMain.java
@@ -7,24 +7,21 @@ public class MCAuthMain extends JavaPlugin {
     @SuppressWarnings("ConstantConditions")
     @Override
     public void onEnable() {
-        Thread thread = new Thread(() -> {
-            saveDefaultConfig();
-            FileConfiguration config = this.getConfig();
+        saveDefaultConfig();
+        FileConfiguration config = this.getConfig();
 
-            String address = config.getString("address", "127.0.0.1");
-            String token = config.getString("token", "");
-            int port = config.getInt("port", 3001);
+        String address = config.getString("address", "127.0.0.1");
+        String token = config.getString("token", "");
+        int port = config.getInt("port", 3001);
 
-            Client client = new Client(address, port, token);
-            MCListener listener = new MCListener(client);
-            AltCommands altCommands = new AltCommands(client);
+        Client client = new Client(address, port, token);
+        MCListener listener = new MCListener(client);
+        AltCommands altCommands = new AltCommands(client);
 
-            getServer().getPluginManager().registerEvents(listener, this);
-            this.getCommand("addalt").setExecutor(altCommands);
-            this.getCommand("remalt").setExecutor(altCommands);
-            this.getCommand("listalts").setExecutor(altCommands);
-            this.getCommand("getalts").setExecutor(altCommands);
-        });
-        thread.start();
+        getServer().getPluginManager().registerEvents(listener, this);
+        this.getCommand("addalt").setExecutor(altCommands);
+        this.getCommand("remalt").setExecutor(altCommands);
+        this.getCommand("listalts").setExecutor(altCommands);
+        this.getCommand("getalts").setExecutor(altCommands);
     }
 }
