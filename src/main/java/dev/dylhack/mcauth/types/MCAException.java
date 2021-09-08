fd226295f8d6104193f9917cd012c11f28f0b623
Dylan Hackworth <dylhack@tutanota.com>
2021-09-08 01:31:37 -0500

    refactor: MCAuthError is now MCAException

--- src/main/java/dev/dylhack/mcauth/types/MCAException.java
+++ src/main/java/dev/dylhack/mcauth/types/MCAException.java
@@ -3,9 +3,10 @@ package dev.dylhack.mcauth.types;
 import java.lang.Exception;
 import org.json.JSONObject;
 
-public class MCAuthError extends Exception {
+public class MCAException extends Exception {
   public final String code;
-  public MCAuthError(JSONObject data) {
+
+  public MCAException(JSONObject data) {
     super(data.getString("message"));
     String errCode = data.getString("errcode");
     this.code = errCode;
