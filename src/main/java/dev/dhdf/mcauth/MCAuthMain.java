813fc35f452b5aedc3a66ca6be1ecc12d9920725
Dylan Hackworth <dylhack@tutanota.com>
2021-09-07 23:05:44 -0500

    feat: Added status print upon enabling MCAuth

--- src/main/java/dev/dhdf/mcauth/MCAuthMain.java
+++ src/main/java/dev/dhdf/mcauth/MCAuthMain.java
@@ -1,5 +1,7 @@
 package dev.dhdf.mcauth;
 
+import java.util.logging.Logger;
+
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,13 +13,33 @@ public class MCAuthMain extends JavaPlugin {
         MCAConfig config = new MCAConfig(this, file);
 
         Client client = new Client(config.host, config.port, config.token);
-        MCListener listener = new MCListener(config, client);
+        MCListener listener = new MCListener(this, config, client);
         AltCommands altCommands = new AltCommands(client);
 
+        // Print out the status of MCAuth
+        this.status(config);
         getServer().getPluginManager().registerEvents(listener, this);
         this.getCommand("addalt").setExecutor(altCommands);
         this.getCommand("remalt").setExecutor(altCommands);
         this.getCommand("listalts").setExecutor(altCommands);
         this.getCommand("getalts").setExecutor(altCommands);
+    }
+
+    private void status(MCAConfig config) {
+        Logger log = this.getLogger();
+        String message = String.format(
+            "MCAuth Configuration\n"
+            + "auth_scheme: %s\n"
+            + "host: %s:%d\n"
+            + "whitelist:",
+            config.authScheme,
+            config.host, config.port
+        );
+        for (String id : config.whitelist) {
+            message += "\n";
+            message += String.format(" - %s", id);
+        }
+
+        log.info(message);
     }
 }
